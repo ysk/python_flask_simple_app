8e3c7f747278b0471197d6cc8a3a14f0b659f42d
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2021-08-30 23:35:12 +0900

    色々試している途中

--- app/app.py
+++ app/app.py
@@ -3,15 +3,26 @@ from flask import Blueprint
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-  title = 'メンバー一覧'
-  members = [
-      {'name':'山田太郎', 'age':35},
-      {'name':'佐藤太郎', 'age':20},
-      {'name':'鈴木太郎', 'age':50},
+    title = 'メンバー一覧'
+    members = [
+      {'id':1, 'name':'山田太郎', 'age':35},
+      {'id':2, 'name':'佐藤太郎', 'age':20},
+      {'id':3, 'name':'鈴木太郎', 'age':50},
       ]
-  return render_template('index.html', title=title, members=members)
+    return render_template('index.html', title=title, members=members)
 
+
+@app.route('/<int:user_id>', methods=['GET'])
+def detail(user_id):
+
+    ##この辺の値はDBに格納
+    user_name = '山田太郎'
+    message   = 'はじめまして山田太郎です。'
+    return render_template('detail.html', user_id=user_id, user_name=user_name, message=message)
+
+##実行
 if __name__ == '__main__':
   app.run()
+
